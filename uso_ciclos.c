8617bea3d10f51bc3b1cdb83a9dda181a3cddd35
Victor Rolack <victorolack@gmail.com>
2020-05-23 02:34:27 -0400

    Definiciones de estructuras de control (while, do while, for)

---
+++ uso_ciclos.c
@@ -0,0 +1,22 @@
+/*int main()
+{
+    int n = 1, acum = 10;
+
+    while(acum > 10)
+    {
+        acum = +n;
+        n++;
+    }
+    return acum;
+}*/
+
+int main()
+{
+    int n = 1, acum = 10;
+
+    do {
+        acum += n;
+        n++;
+    } while(acum < 10);
+    return acum;
+}
